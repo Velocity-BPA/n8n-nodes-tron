daa2c37198aa724d071d39dd099530f3ec0e17ed
John Drefahl <john@velobpa.com>
2026-02-24 04:41:14 -0600

    chore: update .eslintrc.js to v1.0.0

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,15 @@
 /*
- * Copyright (c) Velocity BPA, LLC
+ * Copyright (c) 2026 Velocity BPA, LLC
  * Licensed under the Business Source License 1.1
- * Commercial use requires a separate commercial license.
- * See LICENSE file for details.
  */
 
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 2020,
-    sourceType: 'module',
-  },
-  plugins: ['@typescript-eslint'],
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'prettier',
-  ],
-  env: {
-    node: true,
-    es2020: true,
-  },
+  plugins: ['eslint-plugin-n8n-nodes-base'],
+  extends: ['plugin:n8n-nodes-base/community'],
+  ignorePatterns: ['node_modules/**', 'dist/**'],
   rules: {
-    '@typescript-eslint/no-explicit-any': 'warn',
-    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/no-var-requires': 'off',
-    'no-console': 'off',
+    'n8n-nodes-base/community-package-json-name-still-default': 'off',
   },
-  ignorePatterns: ['dist/', 'node_modules/', '*.js'],
 };
