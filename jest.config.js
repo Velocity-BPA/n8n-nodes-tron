0cdbbab573cc2edb54bf58c294fb4ea7f1403bc2
John Drefahl <john@velobpa.com>
2026-02-24 04:41:17 -0600

    chore: update jest.config.js to v1.0.0

--- jest.config.js
+++ jest.config.js
@@ -1,21 +1,24 @@
 /*
- * Copyright (c) Velocity BPA, LLC
+ * Copyright (c) 2026 Velocity BPA, LLC
  * Licensed under the Business Source License 1.1
- * Commercial use requires a separate commercial license.
- * See LICENSE file for details.
  */
 
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  roots: ['<rootDir>/test'],
-  testMatch: ['**/*.test.ts'],
-  moduleFileExtensions: ['ts', 'js', 'json'],
+  testMatch: ['**/test/**/*.test.ts'],
   collectCoverageFrom: [
     'nodes/**/*.ts',
     'credentials/**/*.ts',
     '!**/*.d.ts',
+    '!**/index.ts',
   ],
   coverageDirectory: 'coverage',
-  coverageReporters: ['text', 'lcov'],
+  coverageReporters: ['text', 'lcov', 'html'],
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  roots: ['<rootDir>'],
+  transform: {
+    '^.+\.ts$': 'ts-jest',
+  },
+  testTimeout: 30000,
 };
